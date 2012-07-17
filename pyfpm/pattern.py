48fdee38ad69fc12b777c19d1a9f27933b550178
Martin Blech <martinblech@gmail.com>
2012-07-16 22:00:31 -0500

    pattern builder and more pattern tests

--- pyfpm/pattern.py
+++ pyfpm/pattern.py
@@ -1,3 +1,5 @@
+import re
+
 class Match(object):
     def __init__(self, ctx=None):
         if ctx is None:
@@ -17,13 +19,15 @@ class Pattern(object):
             if self.bound_name:
                 if ctx is None:
                     ctx = {}
-                if self.bound_name in ctx:
-                    raise AttributeError('pattern bound to duplicate name: %s'
-                            % self.bound_name)
-                ctx[self.bound_name] = other
+                try:
+                    previous = ctx[self.bound_name]
+                    if previous != other:
+                        return None
+                except KeyError:
+                    ctx[self.bound_name] = other
             return Match(ctx)
         return None
-    def __eq__(self, other):
+    def __lshift__(self, other):
         return self.match(other)
 
     def bind(self, name):
@@ -39,6 +43,22 @@ class Pattern(object):
         return RangePattern(self, length)
     def __mul__(self, length):
         return self.set_length(length)
+    def __rmul__(self, length):
+        return self.set_length(length)
+
+    def set_infinite(self):
+        return self.set_length(RangePattern.INFINITE)
+    def __pos__(self):
+        return self.set_infinite()
+
+    def __eq__(self, other):
+        return (self.__class__ == other.__class__ and
+                self.__dict__ == other.__dict__)
+
+    def __repr__(self):
+        return '%s(%s)' % (self.__class__.__name__,
+                ', '.join('='.join(map(str, (k, v))) for (k, v) in
+                    self.__dict__.items() if v))
 
 class AnyPattern(Pattern):
     def _does_match(self, other):
@@ -60,23 +80,26 @@ class InstanceOfPattern(Pattern):
     def _does_match(self, other):
         return isinstance(other, self.cls)
 
+class RegexPattern(Pattern):
+    def __init__(self, regex):
+        super(RegexPattern, self).__init__()
+        if not isinstance(regex, re.RegexObject):
+            regex = re.compile(regex)
+        self.regex = regex
+    # TODO: finish this, must improve Pattern._do_match
+
 class RangePattern(Pattern):
+    INFINITE = -1
     def __init__(self, pattern=AnyPattern(), length=None):
         super(RangePattern, self).__init__()
         self.pattern = pattern
         self._length = length
 
     def set_length(self, length):
-        self._length = length
-        return self
+        return RangePattern(self.pattern, length)
 
     def length(self):
         return self._length
-
-    def set_infinite(self):
-        return self.set_length(-1)
-    def __pos__(self):
-        return self.set_infinite()
 
     def _does_match(self, other):
         if self.pattern.bound_name:
@@ -86,7 +109,7 @@ class RangePattern(Pattern):
             raise ValueError('range length unset')
         if l == 1:
             raise ValueError('range length must be higher than 1')
-        if l != -1 and len(other) != l:
+        if l != RangePattern.INFINITE and len(other) != l:
             return None
         for item in other:
             if not self.pattern._does_match(item):
@@ -106,7 +129,7 @@ class ListPattern(Pattern):
                 return None
             if l == 1:
                 match = pattern.match(remaining[0], ctx)
-            elif l == -1:
+            elif l == RangePattern.INFINITE:
                 match = pattern.match(remaining, ctx)
             else:
                 match = pattern.match(remaining[:l], ctx)
@@ -114,10 +137,44 @@ class ListPattern(Pattern):
                 return None
             if ctx is None:
                 ctx = match.ctx
-            if l != -1:
+            if l != RangePattern.INFINITE:
                 remaining = remaining[l:]
             else:
                 remaining = tuple()
         if len(remaining):
             return None
         return Match(ctx)
+
+class CasePattern(Pattern):
+    def __init__(self, casecls, *initpatterns):
+        super(CasePattern, self).__init__()
+        self.casecls_pattern = InstanceOfPattern(casecls)
+        self.initargs_pattern = ListPattern(*initpatterns)
+
+    def match(self, other, ctx=None):
+        if not self.casecls_pattern.match(other, ctx):
+            return None
+        match = self.initargs_pattern.match(other._case_args, ctx)
+        if match and self.bound_name:
+            match.ctx[self.bound_name] = other
+        return match
+
+def build(*args):
+    arglen = len(args)
+    if arglen > 1:
+        return ListPattern(*map(build, args))
+    if arglen == 0:
+        return AnyPattern()
+    (arg,) = args
+    if isinstance(arg, Pattern):
+        return arg
+    if hasattr(arg, '_case_args'):
+        return CasePattern(arg.__class__,
+                *map(build, arg._case_args))
+    if isinstance(arg, type):
+        return InstanceOfPattern(arg)
+    if isinstance(arg, (tuple, list)):
+        if len(arg) == 0:
+            return RangePattern(length=0)
+        return build(*arg)
+    return EqualsPattern(arg)
--- tests/test_pattern.py
+++ tests/test_pattern.py
@@ -4,59 +4,132 @@ except ImportError:
     import unittest
 
 from pyfpm import pattern
+from pyfpm.caseclass import Case
 
 _m = pattern.Match
 
 _any = pattern.AnyPattern
 class TestAny(unittest.TestCase):
     def test_match_unbound(self):
-        self.assertEquals(_any()==1, _m())
+        self.assertEquals(_any()<<1, _m())
 
     def test_match_int(self):
-        self.assertEquals(_any()%'x'==1, _m({'x': 1}))
+        self.assertEquals(_any()%'x'<<1, _m({'x': 1}))
 
 _eq = pattern.EqualsPattern
 class TestEquals(unittest.TestCase):
     def test_match_unbound(self):
-        self.assertEquals(_eq(1)==1, _m())
+        self.assertEquals(_eq(1)<<1, _m())
 
     def test_match_int(self):
-        self.assertEquals(_eq(1)%'x'==1, _m({'x': 1}))
+        self.assertEquals(_eq(1)%'x'<<1, _m({'x': 1}))
 
     def test_not_match_different_int(self):
-        self.assertFalse(_eq(1)%'x'==2)
+        self.assertFalse(_eq(1)%'x'<<2)
 
     def test_not_match_different_type(self):
-        self.assertFalse(_eq(1)%'x'=='abc')
+        self.assertFalse(_eq(1)%'x'<<'abc')
 
 _iof = pattern.InstanceOfPattern
 class TestInstance(unittest.TestCase):
     def test_match_int(self):
-        self.assertEquals(_iof(int)%'x'==1, _m({'x': 1}))
+        self.assertEquals(_iof(int)%'x'<<1, _m({'x': 1}))
 
     def test_match_different_int(self):
-        self.assertEquals(_iof(int)%'x'==2, _m({'x': 2}))
+        self.assertEquals(_iof(int)%'x'<<2, _m({'x': 2}))
 
     def test_not_match_different_type(self):
-        self.assertFalse(_iof(int)%'x'=='abc')
+        self.assertFalse(_iof(int)%'x'<<'abc')
 
 _r = pattern.RangePattern
 class TestRange(unittest.TestCase):
     def test_match_range_unbound(self):
-        self.assertEquals(_iof(str)*2==('a', 'b'), _m())
+        self.assertEquals(_iof(str)*2<<('a', 'b'), _m())
 
     def test_match_range(self):
-        self.assertEquals(_iof(int)*2%'x'==(1, 2), _m({'x': (1, 2)}))
+        self.assertEquals(_iof(int)*2%'x'<<(1, 2), _m({'x': (1, 2)}))
+
+    # TODO: more tests
 
 _l = pattern.ListPattern
 class TestList(unittest.TestCase):
     def test_match_single_item(self):
-        self.assertEquals(_l(_eq(1)%'x')==[1], _m({'x': 1}))
+        self.assertEquals(_l(_eq(1)%'x')<<[1], _m({'x': 1}))
 
     def test_match_multiple_items(self):
-        self.assertEquals(_l(_eq(1)%'x', _iof(str)%'y')==(1, 'a'),
+        self.assertEquals(_l(_eq(1)%'x', _iof(str)%'y')<<(1, 'a'),
                 _m({'x': 1, 'y': 'a'}))
 
     def test_match_head_tail(self):
-        self.assertEquals(_l(_any()%'head', +_r()%'tail')==(1, 2, 3),
+        self.assertEquals(_l(_any()%'head', +_r()%'tail')<<(1, 2, 3),
                 _m({'head': 1, 'tail': (2, 3)}))
+
+    # TODO: more tests
+
+_c = pattern.CasePattern
+class MyCase(Case):
+    def __init__(self, *args): pass
+
+class TestList(unittest.TestCase):
+    def test_match_single_arg(self):
+        self.assertEquals(_c(MyCase, _eq(1)%'x')<<MyCase(1), _m({'x': 1}))
+
+    def test_match_multiple_args(self):
+        self.assertEquals(_c(MyCase, _eq(1)%'x', _iof(str)*3%'y')<<
+                                                MyCase(1, 'a', 'b', 'c'),
+                _m({'x': 1, 'y': ('a', 'b', 'c')}))
+
+    def test_match_head_tail(self):
+        self.assertEquals(_c(MyCase, _any()%'head', +_r()%'tail')%'x'<<
+                                                MyCase(1, 2, 3),
+                        _m({'head': 1, 'tail': (2, 3), 'x': MyCase(1, 2, 3)}))
+
+    # TODO: more tests
+
+_ = pattern.build
+class TestPBuilder(unittest.TestCase):
+    def test_int(self):
+        self.assertEquals(_(1), _eq(1))
+
+    def test_str(self):
+        self.assertEquals(_('abc'), _eq('abc'))
+
+    def test_class(self):
+        self.assertEquals(_(str), _iof(str))
+
+    def test_case(self):
+        class MyCase(Case): pass
+        self.assertEquals(_(MyCase()), _c(MyCase))
+
+    def test_pattern(self):
+        self.assertEquals(_(_()), _())
+
+    def test_any(self):
+        self.assertEquals(_(), _any())
+
+    def test_nil(self):
+        self.assertEquals(_([]), _r(length=0))
+
+    def test_list(self):
+        self.assertEquals(_(1, str), _l(_(1), _(str)))
+
+class TestOperators(unittest.TestCase):
+    def test_mul(self):
+        self.assertEquals(_()*2, _r(_any(), 2))
+    def test_rmul(self):
+        self.assertEquals(2*_(1), _(1)*2)
+
+    def test_mod(self):
+        self.assertEquals(_(1)%'x', _(1).bind('x'))
+
+    def test_pos(self):
+        self.assertEquals(+_(1), _(1).set_infinite())
+
+class TestMultibind(unittest.TestCase):
+    pattern = _(_()%'x', _()%'x', _()%'y')
+    def test_sameok(self):
+        self.assertEquals(TestMultibind.pattern << (1, 1, 2),
+                _m({'x': 1, 'y': 2}))
+
+    def test_differentfail(self):
+        self.assertFalse(TestMultibind.pattern << (1, 2, 3))
