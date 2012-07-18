a8e340ba8b26c3ff4f814f9ece15585266fc706a
Martin Blech <martinblech@gmail.com>
2012-07-18 17:59:56 -0500

    imported NoMatch exception into the main namespace

--- pyfpm/__init__.py
+++ pyfpm/__init__.py
@@ -1,3 +1,3 @@
 from pattern import build
 from caseclass import case_metacls, Case
-from matcher import Matcher, MatchFunction, handler
+from matcher import Matcher, MatchFunction, handler, NoMatch
