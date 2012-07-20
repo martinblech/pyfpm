7229de425b2d3fb50ad4026cf0fc5814792c63c0
Martin Blech <martinblech@gmail.com>
2012-07-19 19:03:49 -0500

    scala-like pattern syntax parser with pyparsing

---
+++ pyfpm/parser.py
@@ -0,0 +1,85 @@
+from __future__ import print_function
+import inspect
+
+from pyparsing import Literal, Word, Group, Combine, Suppress, OneOrMore,\
+        Forward, Optional, alphas, nums, alphanums, stringEnd, oneOf,\
+        quotedString, dblQuotedString, removeQuotes, delimitedList,\
+        ParseException
+
+from pyfpm import build as _
+
+def Parser(context=None):
+    if context is None:
+        frame = inspect.getouterframes(inspect.currentframe())[1][0]
+        context = frame.f_globals
+
+    # parsing actions
+    def get_type(type_name):
+        try:
+            t = eval(type_name, context)
+        except NameError:
+            raise ParseException('unknown type: %s' % type_name)
+        if not isinstance(t, type):
+            raise ParseException('not a type: %s' % type_name)
+        return t
+
+    def get_named_var(var_name):
+        try:
+            get_type(var_name)
+        except ParseException:
+            return _()%var_name
+        raise ParseException('var name clashes with type: %s' % var_name)
+
+    # begin grammar
+    type_ = Word(alphas, alphanums + '._')('type_').setParseAction(
+            lambda r: get_type(r.type_))
+
+    anon_var = Literal("_")('anon_var').setParseAction(lambda: _())
+
+    named_var = Word(alphas, alphanums + '_')('named_var').setParseAction(
+            lambda r: get_named_var(r.named_var))
+
+    untyped_var = (named_var | anon_var)('untyped_var')
+
+    typed_var = (untyped_var + Suppress(':') + type_)(
+            'typed_var').setParseAction(
+            lambda r: _(r.type_)%r.untyped_var.bound_name)
+
+    var = (typed_var | untyped_var)('var')
+
+    int_const = Combine(Optional('-') + Word(nums))(
+            'int_const').setParseAction(lambda r: int(r.int_const))
+
+    float_const = Combine(Optional('-') + Word(nums) + Literal('.')
+            + Optional(Word(nums)) |
+            Optional('-') + Literal('.') + Word(nums))(
+            'float_const').setParseAction(lambda r: float(r.float_const))
+
+    str_const = (quotedString | dblQuotedString)('str_const').setParseAction(
+            removeQuotes)
+
+    const = (float_const | int_const | str_const)(
+            'const').setParseAction(lambda r: _(r.const))
+
+    scalar = (var | const)('scalar')
+
+    pattern = Forward()
+
+    head_tail = (scalar + Suppress('::') + pattern)(
+            'head_tail').setParseAction(lambda r: r[0] + r[1])
+
+    list_item = (scalar | pattern)('list_item')
+
+    full_list = (Suppress('[') + Optional(delimitedList(list_item)) +
+            Suppress(']'))('full_list').setParseAction(lambda r: _(list(r)))
+
+    list_ = (head_tail | full_list)('list')
+
+    pattern << (list_ | scalar)('pattern')
+    # end grammar
+
+    def parse(expression):
+        return pattern.parseString(expression, parseAll=True)[0]
+
+    parse.context = context
+    return parse
---
+++ tests/test_parser.py
@@ -0,0 +1,99 @@
+import unittest
+
+from pyfpm import parser, build as _
+
+class TestParser(unittest.TestCase):
+    def setUp(self):
+        self.parse = parser.Parser()
+
+    def test_automatic_context(self):
+        self.assertEquals(self.parse.context, globals())
+
+    def test_manual_context(self):
+        obj = 'my local string'
+        p = parser.Parser({'x': obj})
+        self.assertEquals(p.context['x'], obj)
+
+    def test_anon_var(self):
+        pattern = self.parse('_')
+        self.assertEquals(pattern, _())
+
+    def test_named_var(self):
+        pattern = self.parse('x')
+        self.assertEquals(pattern, _()%'x')
+
+    def test_type_annotation_space_irrelevance(self):
+        self.assertEquals(self.parse('_:str'), self.parse('_ : str'))
+
+    def test_typed_anon_var(self):
+        pattern = self.parse('_:str')
+        self.assertEquals(pattern, _(str))
+
+    def test_typed_named_var(self):
+        pattern = self.parse('x:str')
+        self.assertEquals(pattern, _(str)%'x')
+
+    def test_nested_type_var(self):
+        pattern = self.parse('x:unittest.TestCase')
+        self.assertEquals(pattern, _(unittest.TestCase)%'x')
+
+    def test_named_var_clash(self):
+        for expr in ('str', 'object:str'):
+            try:
+                pattern = self.parse(expr)
+                self.fail()
+            except parser.ParseException:
+                pass
+
+    def test_fail_with_unknown_type(self):
+        try:
+            self.parse('_:fdsa')
+            self.fail('fdsa is undefined')
+        except parser.ParseException:
+            pass
+
+    def test_fail_with_known_nontype(self):
+        try:
+            self.parse('_:unittest')
+            self.fail('unittest is not a type')
+        except parser.ParseException:
+            pass
+
+    def test_int_constant(self):
+        self.assertTrue(self.parse('1'), _(1))
+        self.assertTrue(self.parse('-1'), _(-1))
+        try:
+            self.parse('- 1')
+            self.fail('bad integer')
+        except parser.ParseException:
+            pass
+
+    def test_float_constant(self):
+        self.assertEquals(self.parse('1.'), _(1.))
+        self.assertEquals(self.parse('.5'), _(.5))
+        self.assertEquals(self.parse('1.5'), _(1.5))
+        self.assertEquals(self.parse('-1.'), _(-1.))
+        self.assertEquals(self.parse('-.5'), _(-.5))
+        self.assertEquals(self.parse('-1.5'), _(-1.5))
+        try:
+            self.parse('1 . 0')
+            self.fail('bad float')
+        except parser.ParseException:
+            pass
+
+    def test_str_constant(self):
+        self.assertEquals(self.parse('"abc"'), _('abc'))
+        self.assertEquals(self.parse("'abc'"), _('abc'))
+
+    def test_head_tail(self):
+        self.assertEquals(self.parse('head :: tail'), _()%'head' + _()%'tail')
+        self.assertEquals(self.parse('a :: b :: c'),
+                _()%'a' + _()%'b' + _()%'c')
+        self.assertEquals(self.parse('a :: b :: c :: d'),
+                _()%'a' + _()%'b' + _()%'c' + _()%'d')
+
+    def test_explicit_list(self):
+        self.assertEquals(self.parse('[]'), _([]))
+        self.assertEquals(self.parse('[1]'), _([1]))
+        self.assertEquals(self.parse('[_, x:int]'), _(_(), _(int)%'x'))
+        self.assertEquals(self.parse('[_, []]'), _(_(), _([])))
