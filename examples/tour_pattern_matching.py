258f6803ad43492842188ae3ed3214f6b4fe3508
Martin Blech <martinblech@gmail.com>
2012-07-20 12:08:31 -0500

    updated examples for the new pattern syntax parser

--- examples/tour_pattern_matching.py
+++ examples/tour_pattern_matching.py
@@ -8,21 +8,21 @@ from pyfpm import Matcher as M
 from pyfpm import build as _
 
 print('-'*80)
-matchTest = M(
-        (_(1), lambda: 'one'),
-        (_(2), lambda: 'two'),
-        (_(), lambda: 'many')
-        )
+matchTest = M([
+        ('1', lambda: 'one'),
+        ('2', lambda: 'two'),
+        ('_', lambda: 'many')
+        ])
 print(matchTest(1))
 print(matchTest(2))
 print(matchTest(3))
 
 print('-'*80)
-matchTest = M(
-        (_(1), lambda: 'one'),
-        (_("two"), lambda: 2),
-        (_(int)%'y', lambda y: 'scala.Int')
-        )
+matchTest = M([
+        ('1', lambda: 'one'),
+        ('"two"', lambda: 2),
+        ('y:int', lambda y: 'scala.Int')
+        ])
 print(matchTest(1))
 print(matchTest("two"))
 print(matchTest(3))
