fa28d004f9c5ec9af6b3f04bb1a011b99af77398
Martin Blech <martinblech@gmail.com>
2012-08-04 23:23:44 -0500

    specifying author email, setup.py complains otherwise

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ import pyfpm
 setup(name='pyfpm',
         version=pyfpm.__version__,
         author=pyfpm.__author__,
+        author_email='martinblech@gmail.com',
         url='https://github.com/martinblech/pyfpm',
         description='Scala-like functional pattern matching in Python.',
         long_description="""`pyfpm` stands for PYthon Functional Pattern
