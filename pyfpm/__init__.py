e977bdc0e79c120ccfa6e9f759bdabceaa81b519
Martin Blech <martinblech@gmail.com>
2012-08-04 23:19:33 -0500

    completed package metadata an license information

--- pyfpm/__init__.py
+++ pyfpm/__init__.py
@@ -3,3 +3,4 @@
 __version__ = '0.1.2-dev'
 __author__ = 'Martin Blech'
 __copyright__ = '2012, ' + __author__
+__license__ = 'MIT'
--- setup.py
+++ setup.py
@@ -4,10 +4,19 @@ from distutils.core import setup
 import pyfpm
 
 setup(name='pyfpm',
-      version=pyfpm.__version__,
-      description='Scala-like functional pattern matching in Python.',
-      author=pyfpm.__author__,
-      url='https://github.com/martinblech/pyfpm',
-      packages=['pyfpm'],
-      requires=['pyparsing'],
-     )
+        version=pyfpm.__version__,
+        author=pyfpm.__author__,
+        url='https://github.com/martinblech/pyfpm',
+        description='Scala-like functional pattern matching in Python.',
+        long_description="""`pyfpm` stands for PYthon Functional Pattern
+        Matching. It's been heavily inspired by the Pattern Matching and Case
+        Classes implementation in Scala.""",
+        classifiers=[
+            'Development Status :: 4 - Beta',
+            'Intended Audience :: Developers',
+            'License :: OSI Approved :: MIT License',
+            'Topic :: Software Development :: Libraries',
+            ],
+        packages=['pyfpm'],
+        requires=['pyparsing'],
+        )
