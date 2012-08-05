8c11d1c0b7f96c9960834efdc1c9d0d124ccb274
Martin Blech <martinblech@gmail.com>
2012-08-04 22:06:24 -0500

    fixed author ref in setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import pyfpm
 setup(name='pyfpm',
       version=pyfpm.__version__,
       description='Scala-like functional pattern matching in Python.',
-      author=pyfpm.__version__,
+      author=pyfpm.__author__,
       url='https://github.com/martinblech/pyfpm',
       packages=['pyfpm'],
       requires=['pyparsing'],
