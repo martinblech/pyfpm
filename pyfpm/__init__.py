622d7bb603cf3a69672ed0f39a8fcfc796a68aa5
Martin Blech <martinblech@gmail.com>
2012-08-04 23:27:47 -0500

    -dev suffix to version number

--- pyfpm/__init__.py
+++ pyfpm/__init__.py
@@ -1,6 +1,6 @@
 """This module provides Scala-like functional pattern matching in Python."""
 
-__version__ = '0.1.3'
+__version__ = '0.1.3-dev'
 __author__ = 'Martin Blech'
 __copyright__ = '2012, ' + __author__
 __license__ = 'MIT'
